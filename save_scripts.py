10f46bdfeea6492c6c6ed71766947b204676cd4c
pj6444 <pj6444@gmail.com>
2017-11-15 17:33:18 -0600

    Add python script to scrape all movie scripts

---
+++ save_scripts.py
@@ -0,0 +1,15 @@
+import json
+import requests
+from urllib import pathname2url
+with open('script_links.json', 'r') as f:
+    script_list = json.loads(f.read())
+
+    for script in script_list:
+        name = script['name']
+        url = script['url']
+        print name, url
+        if url[-5:] != '.html':
+            continue
+        response = requests.get(url)
+        with open('./scripts/' + pathname2url(name.replace(' ', '_')) + '.html', 'w') as script_file:
+            script_file.write(response.text.encode('utf-8'))
