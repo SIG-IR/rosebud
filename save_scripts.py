6d39bb479e4dc55002143241e21f03e8f0ccfc54
Ben Pankow <pankow.ben@gmail.com>
2018-02-06 19:52:37 -0600

    Convert save_scripts.py to python3

--- save_scripts.py
+++ save_scripts.py
@@ -1,6 +1,10 @@
 import json
 import requests
-from urllib import pathname2url
+from urllib.request import pathname2url
+import os
+
+if not os.path.exists('./scripts/'):
+    os.makedirs('./scripts/')
 with open('script_links.json', 'r') as f:
     script_list = json.loads(f.read())
 
@@ -11,5 +15,5 @@ with open('script_links.json', 'r') as f:
         if url[-5:] != '.html':
             continue
         response = requests.get(url)
-        with open('./scripts/' + pathname2url(name.replace(' ', '_')) + '.html', 'w') as script_file:
+        with open('./scripts/' + pathname2url(name.replace(' ', '_')) + '.html', 'wb') as script_file:
             script_file.write(response.text.encode('utf-8'))
